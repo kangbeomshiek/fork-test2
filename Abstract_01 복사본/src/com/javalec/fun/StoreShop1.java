1f11c63324e2c73abb1f3714dd298640478d2abb
Kanbeomshiek <tvzz1105@naver.com>
2023-12-08 15:19:35 +0900

    금일 수업자료

---
+++ Abstract_01 복사본/src/com/javalec/fun/StoreShop1.java
@@ -0,0 +1,23 @@
+package com.javalec.fun;
+
+public class StoreShop1 extends StoreHQ {
+
+	@Override
+	public void orderKim() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void orderBu() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void orderBi() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
